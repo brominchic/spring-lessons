e5434f7b258e849b57858e0fad77ff3f6c1b1d4e
timur <timur.petrov.work@gmail.com>
2024-08-17 19:21:53 +0500

    lombok и application properties

--- src/main/java/com/example/spring/controller/TestController.java
+++ src/main/java/com/example/spring/controller/TestController.java
@@ -2,20 +2,22 @@ package com.example.spring.controller;
 
 import com.example.spring.component.TestComponent;
 import com.example.spring.users.User;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
+@RequiredArgsConstructor
 @RequestMapping("/main")
 @RestController
 public class TestController {
 
     private final TestComponent testComponent;
 
-    public TestController(TestComponent testComponent) {
-        this.testComponent = testComponent;
-    }
-
     @PostMapping
-    public User getMain(@RequestBody User user) {
+    public User getMain(@RequestBody User user, HttpServletRequest request) {
+        log.info("request url is {}", request.getRequestURI());
         return user;
     }
 
