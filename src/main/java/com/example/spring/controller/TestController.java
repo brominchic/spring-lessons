b38208c85acd5a0c64e8d3a59fed031127e747a0
brominchic <132040687+brominchic@users.noreply.github.com>
2024-08-17 18:58:33 +0500

    Feature/mergeRequests (#1)
    Реализуем post методы в  новом контроллере. как минимум один метод должен возвращать объект и принимать JSON
    
    Использовать аннотации
    @RequestBody - в POST
    @PathVariable - в любом запросе
    @RequestParam - в любом запросе
    
    для реализованных методов подготовить коллекцию postman
    
    контрольные вопросы
    1. какие методы HTTP запросов ты знаешь и для чего они применяются
    2. какие составные части HTTP запроса
    3. что такое Application Context в Spring
    4. какими аннотациями назначаются типы запросов в каком компоненте
    5. как работают аннотации RestController, RequestMapping, Component, RequestBody, PathVariable, RequestParam

---
+++ src/main/java/com/example/spring/controller/TestController.java
@@ -0,0 +1,33 @@
+package com.example.spring.controller;
+
+import com.example.spring.component.TestComponent;
+import com.example.spring.users.User;
+import org.springframework.web.bind.annotation.*;
+
+@RequestMapping("/main")
+@RestController
+public class TestController {
+
+    private final TestComponent testComponent;
+
+    public TestController(TestComponent testComponent) {
+        this.testComponent = testComponent;
+    }
+
+    @PostMapping
+    public User getMain(@RequestBody User user) {
+        return user;
+    }
+
+
+    @GetMapping("/{name}/{lastname}")
+    public String getMainByPath(@PathVariable(name = "name") String firstname, @PathVariable String lastname) {
+        return testComponent.getPage(firstname, lastname);
+    }
+
+    @GetMapping
+    public String getMainByRequestParam(@RequestParam String firstname, @RequestParam String lastname) {
+        return testComponent.getPage(firstname, lastname);
+    }
+
+}
