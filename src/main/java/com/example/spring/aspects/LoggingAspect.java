626c4b7d2d613b6aecc888b806053bb17aa98d43
brominchic <132040687+brominchic@users.noreply.github.com>
2024-08-26 21:08:14 +0500

    логирующий аспект, который срабатывает только вокруг TaskProcessor-ов (#3)
    логирующий аспект, который срабатывает только вокруг TaskProcessor-ов

---
+++ src/main/java/com/example/spring/aspects/LoggingAspect.java
@@ -0,0 +1,31 @@
+package com.example.spring.aspects;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j
+public class LoggingAspect {
+
+    @Pointcut("within(com.example.spring.component.TaskProcessor+)")
+    public void logTaskProcessor() {
+    }
+
+    @Before("logTaskProcessor()")
+    public void beforeLogTaskProcessor(JoinPoint joinPoint) {
+        log.info("Requested {}\n {}\n {}", joinPoint.getArgs(), joinPoint.getKind(), joinPoint.getSignature());
+    }
+
+    @After("logTaskProcessor()")
+    public void afterLogTaskProcessor(JoinPoint joinPoint) {
+        log.info("Finished {}", joinPoint.getTarget().getClass().getName());
+    }
+
+
+}
