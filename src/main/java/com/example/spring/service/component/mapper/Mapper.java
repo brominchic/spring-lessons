567614f642cee8aca0e4cb3a0b8a6f6891401048
timur <timur.petrov.work@gmail.com>
2024-11-10 00:33:40 +0500

    Чистка

--- src/main/java/com/example/spring/service/component/mapper/Mapper.java
+++ src/main/java/com/example/spring/service/component/mapper/Mapper.java
@@ -1,16 +1,13 @@
 package com.example.spring.service.component.mapper;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.stereotype.Service;
-
-import java.io.IOException;
 
 @Service
 public interface Mapper<D, E> {
-    D entityToDto(E entity) throws IOException;
+    D entityToDto(E entity) throws Exception;
 
     E dtoToEntity(D dto);
 
-    D stringToDto(String string) throws JsonProcessingException;
+    D stringToDto(String string) throws Exception;
 
 }
--- src/main/java/com/example/spring/service/jpa/FileComponent.java
+++ src/main/java/com/example/spring/service/jpa/FileComponent.java
@@ -3,11 +3,7 @@ package com.example.spring.service.jpa;
 import com.example.spring.model.entity.FileEntity;
 import com.example.spring.model.entity.UserEntity;
 import com.example.spring.repositories.FileRepository;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
@@ -22,17 +18,20 @@ public class FileComponent {
 
     @Transactional
     public Long create(Long userId, MultipartFile file) throws IOException {
-        FileEntity entity = FileEntity.builder().userEntity(UserEntity.builder().id(userId).build()).fileData(file.getBytes()).filename(file.getOriginalFilename()).build();
+        FileEntity entity = FileEntity.builder()
+                .userEntity(UserEntity
+                        .builder()
+                        .id(userId)
+                        .build())
+                .fileData(file.getBytes())
+                .filename(file.getOriginalFilename())
+                .build();
         return repository.save(entity).getId();
     }
 
     @Transactional
-    public HttpEntity<byte[]> getFile(Long id, HttpServletResponse response) throws IOException {
-        FileEntity file = repository.findById(id).orElseThrow();
-        byte[] resource = file.getFileData();
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
-        response.setHeader("Content-Disposition", "attachment; filename=" + file.getFilename());
-        return new HttpEntity<byte[]>(resource, headers);
+    public byte[] getFile(String filename) throws IOException {
+        byte[] resource = repository.findByFilename(filename).orElseThrow().getFileData();
+        return resource;
     }
 }
