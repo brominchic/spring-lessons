bfd1872ade6b8c2e1a690b5e61a010ed75e11ba7
brominchic <132040687+brominchic@users.noreply.github.com>
2024-11-10 15:56:28 +0500

    убрал лишние методы (#8)

--- src/main/java/com/example/spring/service/component/mapper/Mapper.java
+++ src/main/java/com/example/spring/service/component/mapper/Mapper.java
@@ -1,6 +1,5 @@
 package com.example.spring.service.component.mapper;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -8,7 +7,5 @@ public interface Mapper<D, E> {
     D entityToDto(E entity);
 
     E dtoToEntity(D dto);
-
-    D stringToDto(String string) throws JsonProcessingException;
 
 }
