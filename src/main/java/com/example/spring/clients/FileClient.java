984a39b3eba229f3d9d5e9de07cc67577267a097
timur <timur.petrov.work@gmail.com>
2024-12-01 00:01:20 +0500

    Исправление теста

--- src/main/java/com/example/spring/clients/FileClient.java
+++ src/main/java/com/example/spring/clients/FileClient.java
@@ -1,22 +1,20 @@
 package com.example.spring.clients;
 
 import com.example.spring.config.FeignConfig;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
 
-@FeignClient(
-        name = "FileClient",
-        url = "http://localhost:9999/example-application/jpa/files",
-        configuration = FeignConfig.class
-)
+@FeignClient(name = "FileClient", url = "http://localhost:9999/example-application/jpa/files", configuration = FeignConfig.class)
 public interface FileClient {
-    @PostMapping("/upload")
-    Long uploadFile(HttpEntity entity);
+    @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    Long uploadFile(@RequestPart("file") MultipartFile file, @RequestPart("userId") Integer Id);
 
     @GetMapping("/download/{id}")
-    HttpEntity<byte[]> getFile(@PathVariable Long id, HttpServletResponse response);
+    HttpEntity<byte[]> getFile(@PathVariable Long id);
 }
--- src/test/java/com/example/spring/service/jpa/FeignIntegrationFileTest.java
+++ src/test/java/com/example/spring/service/jpa/FeignIntegrationFileTest.java
@@ -1,8 +1,8 @@
 package com.example.spring.service.jpa;
 
+import com.example.spring.clients.FileClient;
 import com.example.spring.model.entity.UserEntity;
 import com.example.spring.repositories.UserRepository;
-import com.example.spring.service.feign.FileFeignService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,6 +10,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.core.io.Resource;
+import org.springframework.http.HttpEntity;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @EnableFeignClients
@@ -19,30 +24,34 @@ class FeignIntegrationFileTest extends SpringBootApplicationTest {
     @Autowired
     UserRepository repository;
     @Autowired
-    private FileFeignService service;
+    private FileClient client;
     @Value("classpath:antonosov.txt")
     private Resource classPathResource;
 
     /*
-    за интеграционный тест загрузить файл в БД через POST используя restTemplate,
-    скачать обратно также через GET, проверить соответствие.
+    за интеграционный тест загрузить файл в БД используя feign,
+    скачать обратно , проверить соответствие.
     файл должен храниться в resource
     */
     @Test
     void testUploadAndDownloadFile() throws Exception {
-        // Загрузка пользователя в бд
         UserEntity userEntity = UserEntity.builder().
                 id(1L).
                 fullName("dva").
                 totalBalance(100L).
                 build();
         repository.save(userEntity);
-        // URL
-        String uploadUrl = "http://localhost:" + port + "/example-application/jpa/files/upload";
-        String downloadUrl = "http://localhost:" + port + "/example-application/jpa/files/download/1";
-        // Отправка запроса
-        service.create("antonosov.txt");
-
+        String name = "antonosov.txt";
+        String originalFileName = "antonosov.txt";
+        String contentType = "text/plain";
+        byte[] content = classPathResource.getContentAsByteArray();
+        MultipartFile result = new MockMultipartFile(name,
+                originalFileName, contentType, content);
+        // Отправка файла
+        client.uploadFile(result, 1);
+        // Получение файла
+        HttpEntity<byte[]> request = client.getFile(1L);
+        assertArrayEquals(content, request.getBody());
     }
 
 }
